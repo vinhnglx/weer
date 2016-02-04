e965a0dadf9e2c35fd451c092c73c56214639002
Vinh Nguyen <vinh.nglx@gmail.com>
2016-02-05 01:24:43 +0700

    Require webmock and support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'webmock/rspec'
 require 'tempo'
+require 'byebug'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+Dir[__dir__ + '/support/**/*.rb'].each { |f| require f }
