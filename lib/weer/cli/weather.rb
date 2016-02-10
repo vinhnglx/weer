7e681fa949264bb4cfc6dece28e9cd97b3bb0588
Stanislav Fesenko <iamdeuterium@gmail.com>
2016-02-10 19:58:06 +0300

    Fix titles

--- lib/weer/cli/weather.rb
+++ lib/weer/cli/weather.rb
@@ -24,20 +24,20 @@ module Weer
         raise FakeURLInvalid if response['query']['results'].nil?
         forecasts = wetter.forecast response
 
-        puts Rainbow("============= The forecast(#{options[:temperature]}) of #{options[:city].upcase!} in the next coming days =============").green
+        puts Rainbow(" The forecast(#{options[:temperature]}) of #{options[:city].upcase!} in the next coming days ".center(80, '=')).green
         puts Rainbow(wetter.parse_forecast forecasts, temperature).orange
 
         if options[:all]
           wind = wetter.wind response
 
           puts "\n"
-          puts Rainbow("========================= The wind power of #{Date.today.to_s} =========================").green
+          puts Rainbow(" The wind power of #{Date.today.to_s} ".center(80, '=')).green
           puts Rainbow(wetter.parse_wind wind).orange
 
           atmosphere = wetter.atmosphere response
 
           puts "\n"
-          puts Rainbow("========================= The atmosphere of #{Date.today.to_s} =========================").green
+          puts Rainbow(" The atmosphere of #{Date.today.to_s} ".center(80, '=')).green
           puts Rainbow(wetter.parse_atmosphere atmosphere).orange
         end
       end
